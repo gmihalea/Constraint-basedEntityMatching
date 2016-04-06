3f7052646f7e8fcbb7db8344a4c335cd296789ac
gmihalea <geanina.mihalea@gmail.com>
2016-04-06 18:57:43 +0300

    removed TODOs

--- src/Mentee.java
+++ src/Mentee.java
@@ -6,7 +6,6 @@ import java.util.List;
  * Created by gmihalea on 3/15/2016.
  */
 public class Mentee extends Entity {
-    //TODO add the specific keys for Mentee in the attribute hashMap
 
     public Mentee(HashMap<String, ArrayList<String>> attributes){
         super(attributes);
--- src/Mentor.java
+++ src/Mentor.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Created by gmihalea on 3/15/2016.
  */
 public class Mentor extends Entity {
-    //TODO add the specific keys for Mentor in the attribute HashMap
+
     public Mentor(HashMap<String, ArrayList<String>> attributes){
         super(attributes);
     }
