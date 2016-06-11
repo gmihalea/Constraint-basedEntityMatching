9c1e6c8fc7c8214194564691307cc995f0282b4b
gmihalea <geanina.mihalea@gmail.com>
2016-06-11 22:07:28 +0300

    implemented file chooser for constraints

--- src/client/MainForm.java
+++ src/client/MainForm.java
@@ -2,6 +2,7 @@ package client;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -50,22 +51,48 @@ public class MainForm extends JFrame{
         /**
          * Labels
          */
-        JLabel uploadMentors = new JLabel("Upload Mentors");
-        uploadMentors.setBounds(30, 30, 150, 20);
+        JLabel mentors = new JLabel("Mentors");
+        mentors.setBounds(30, 30, 150, 20);
 
-        JLabel uploadMentees = new JLabel("Upload Mentees");
-        uploadMentees.setBounds(250, 30, 150, 20);
+        JLabel mentees = new JLabel("Mentees");
+        mentees.setBounds(30, 100, 150, 20);
+
+        JLabel constraintsLabel = new JLabel("Constraints");
+        constraintsLabel.setBounds(30, 170, 150, 20);
 
         JLabel mentorsFileName = new JLabel("");
-        mentorsFileName.setBounds(30, 80, 150, 20);
+        mentorsFileName.setBounds(150, 50, 150, 20);
+        mentorsFileName.setForeground(Color.GRAY);
 
         JLabel menteesFileName = new JLabel("");
-        menteesFileName.setBounds(250, 80, 150, 20);
+        menteesFileName.setBounds(150, 120, 150, 20);
+        menteesFileName.setForeground(Color.GRAY);
 
-        add(uploadMentors);
-        add(uploadMentees);
+        JLabel constraintsFileName = new JLabel("");
+        constraintsFileName.setBounds(150, 190, 150, 20);
+        constraintsFileName.setForeground(Color.GRAY);
+
+        JLabel mentorsFileDim = new JLabel("");
+        mentorsFileDim.setBounds(250, 50, 150, 20);
+        mentorsFileDim.setForeground(Color.GRAY);
+
+        JLabel menteesFileDim = new JLabel("");
+        menteesFileDim.setBounds(250, 120, 150, 20);
+        menteesFileDim.setForeground(Color.GRAY);
+
+        JLabel constraintsFileDim = new JLabel("");
+        constraintsFileDim.setBounds(250, 190, 150, 20);
+        constraintsFileDim.setForeground(Color.GRAY);
+
+        add(mentors);
+        add(mentees);
         add(mentorsFileName);
         add(menteesFileName);
+        add(mentorsFileDim);
+        add(menteesFileDim);
+        add(constraintsLabel);
+        add(constraintsFileName);
+        add(constraintsFileDim);
 
         /**
          * Buttons
@@ -74,19 +101,24 @@ public class MainForm extends JFrame{
         browseMentors.setBounds(30, 50, 110, 25);
 
         JButton browseMentees = new JButton("Browse");
-        browseMentees.setBounds(250, 50, 110, 25);
+        browseMentees.setBounds(30, 120, 110, 25);
+
+        JButton browseConstraints = new JButton("Browse");
+        browseConstraints.setBounds(30, 190, 110, 25);
 
         add(browseMentors);
         add(browseMentees);
+        add(browseConstraints);
 
         /**
          * Actions
          */
-        browseMentors.addActionListener(e -> selectFile(mentorsFileName));
-        browseMentees.addActionListener(e -> selectFile(menteesFileName));
+        browseMentors.addActionListener(e -> selectFile(mentorsFileName, mentorsFileDim));
+        browseMentees.addActionListener(e -> selectFile(menteesFileName, menteesFileDim));
+        browseConstraints.addActionListener(e -> selectFile(constraintsFileName, constraintsFileDim));
     }
 
-    public void selectFile(final JLabel label) {
+    public void selectFile(final JLabel nameLabel, final JLabel dimLabel) {
         JFileChooser chooser = new JFileChooser();
 
         // Permit only CSV files.
@@ -96,8 +128,9 @@ public class MainForm extends JFrame{
         if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
             File f = chooser.getSelectedFile();
 
-            label.setText(f.getName() + " ... " + (f.length() / 1000) + "kB");
-            //textField_1.setText(selected.length());
+            nameLabel.setText(f.getName());
+            dimLabel.setText(((float)f.length() / (float)1000) + "kB");
+
             // read  and/or display the file somehow. ....
         } else {
             // user changed their mind
