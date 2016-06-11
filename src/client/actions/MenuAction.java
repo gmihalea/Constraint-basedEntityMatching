540c8531aa3997353bba6bbbd39c6dd5ba2be1dc
gmihalea <geanina.mihalea@gmail.com>
2016-06-12 00:57:46 +0300

    added method descriptions

--- src/client/actions/MenuAction.java
+++ src/client/actions/MenuAction.java
@@ -10,12 +10,19 @@ import java.util.ArrayList;
  */
 public class MenuAction {
 
+    /**
+     * Action that set all labels to empty string
+     * @param labels the list of labels that should be set to empty string
+     */
     public static void newAction(final ArrayList<JLabel> labels) {
         for(JLabel label : labels) {
             label.setText(Constants.EMPTY_STRING);
         }
     }
 
+    /**
+     * Action that closes the entire application
+     */
     public static void exitAction() {
         System.exit(0);
     }
