285f396952afcbc27d6cad084cb8cf484c17024f
gmihalea <geanina.mihalea@gmail.com>
2016-06-11 23:43:32 +0300

    rearranged the constants

--- src/client/util/Constants.java
+++ src/client/util/Constants.java
@@ -11,6 +11,8 @@ public class Constants {
     public static final int DISTANCE_HORIZONTAL_BETWEEN_LAYERS = 130;
     public static final int DISTANCE_BUTTON_LABEL = 20;
 
+    public static final int K = 1000;
+
     public static final int FORM_WIDTH = 600;
     public static final int FORM_HEIGHT = 400;
 
@@ -21,19 +23,19 @@ public class Constants {
     public static final String EMPTY_STRING = "";
     public static final String MENTORS = "Mentors";
     public static final String MENTEES = "Mentees";
-    public static final String CONSTRAINTS = "Constraints";
     public static final String BROWSE = "Browse";
     public static final String TITLE = "MatchMe";
     public static final String FILE = "File";
     public static final String HELP = "Help";
     public static final String NEW = "New";
     public static final String EXIT = "Exit";
-    public static final String GETTING_STARTED = "Getting started";
     public static final String ABOUT = "About";
+    public static final String KB = "Kb";
+    public static final String CONSTRAINTS = "Constraints";
+    public static final String GETTING_STARTED = "Getting started";
 
     public static final String FILTER_DESCRIPTION = "CSV FILES";
     public static final String FILE_EXTENSTION = "csv";
 
-    public static final int K = 1000;
-    public static final String KB = "Kb";
+
 }
